61ec3b5ecc6fccd30dae65c4026b05bf82c5ade4
Théo Uzan <theo.uzan@hotmail.fr>
2025-01-09 16:11:02 +0100

    adding goal component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,34 @@
-function App() {
+import Header from "./components/Header";
+import { MainGoal } from "./components/MainGoal";
+
+const App = () => {
   return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
+    <div id="app">
+      <Header />
+      <h1>Time to Practice!</h1>
+      <p>
+        Build a <code>&lt;MainGoal&gt;</code> component and insert it below this
+        text.
+      </p>
+      <p>
+        Your <code>&lt;MainGoal&gt;</code> component should simply output some
+        text that describes your main course goal (e.g., &quot;My main goal:
+        Learn React in great detail&quot;).
+      </p>
+      <p>
+        <strong>Important:</strong> You custom component must contain the text
+        &quot;My main goal:&quot;
+      </p>
+      <p>
+        <strong>Also important:</strong> For the automatic checks to succeed,
+        you <strong>must export</strong> your custom component function! Not as
+        a default but simply by adding the <code>export</code> keyword in front
+        of your function (e.g., <code>export function YOUR_COMPONENT_NAME</code>
+        ).
+      </p>
+      <MainGoal />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/MainGoal.jsx
@@ -0,0 +1,7 @@
+export const MainGoal = (props) => {
+  return (
+    <div id="main-goal">
+      <p>My main goal: React refresher</p>
+    </div>
+  );
+};
